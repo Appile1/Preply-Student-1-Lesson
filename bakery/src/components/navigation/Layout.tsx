6b696875cd0c43b0c1351911ddd283f5387e2466
Appile1 <ahmadsiddiquej3@gmail.com>
2025-09-03 22:49:47 +0500

    added the avatar based on if user is logged in or not
    added detailed comments

--- bakery/src/components/navigation/Layout.tsx
+++ bakery/src/components/navigation/Layout.tsx
@@ -1,22 +1,85 @@
-import {NavLink, Outlet} from "react-router-dom";
-import {Paths} from "../../utils/paths.ts";
-
+import React from "react";
+import { NavLink, Outlet } from "react-router-dom";
+import { Paths } from "../../utils/paths.ts";
+import Avatar from "@mui/material/Avatar";
+import { useSelector } from "react-redux";
 
 const Layout = () => {
-    return (
-        <div>
-            <nav>
-                <ul className={'nav-list'}>
-                    <NavLink to={Paths.HOME}><li>Home</li></NavLink>
-                    <NavLink to={Paths.CART}><li>ShoppingCart</li></NavLink>
-                    <NavLink to={Paths.CUSTOMERS}><li>Customers</li></NavLink>
-                    <NavLink to={Paths.ORDERS}><li>Orders</li></NavLink>
-                    <NavLink to={Paths.PRODUCTS}><li>Products</li></NavLink>
-                </ul>
-            </nav>
-            <Outlet/>
+  /**
+   * useSelector:
+   * - React-Redux hook to access the Redux store state.
+   * - Here we extract the "authUser" string from state.auth.
+   * - If no user is logged in, authUser will be an empty string "".
+   */
+  const authUser = useSelector(
+    (state: { auth: { authUser: string } }) => state.auth.authUser
+  );
+
+  return (
+    <div>
+      {/* ------------------ Navigation Section ------------------ */}
+      {/* 
+        This <nav> holds the main navigation menu for the app.
+        Each <NavLink> is a React Router link that changes the route.
+        "Paths" is a helper object that stores route constants (cleaner than hardcoding paths).
+        When clicked, each NavLink will render the respective page inside <Outlet>.
+      */}
+      <nav>
+        <ul className="nav-list">
+          <NavLink to={Paths.HOME}>
+            <li>Home</li>
+          </NavLink>
+          <NavLink to={Paths.CART}>
+            <li>ShoppingCart</li>
+          </NavLink>
+          <NavLink to={Paths.CUSTOMERS}>
+            <li>Customers</li>
+          </NavLink>
+          <NavLink to={Paths.ORDERS}>
+            <li>Orders</li>
+          </NavLink>
+          <NavLink to={Paths.PRODUCTS}>
+            <li>Products</li>
+          </NavLink>
+        </ul>
+      </nav>
+
+      {/* ------------------ User Info Section ------------------ */}
+      {/* 
+        If "authUser" is not empty, it means a user is logged in.
+        - We show the username in plain text.
+        - We also show a Material-UI Avatar component:
+          → Displays the first character of the user's name.
+          → Example: "Ahmad" → "A".
+        - Flexbox is used to align the name + avatar to the right corner.
+      */}
+      {authUser && (
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "flex-end", // push content to the right
+            padding: "10px",
+            gap: "10px", // space between name and avatar
+          }}
+        >
+          {/* Show the full user name */}
+          <span>{authUser}</span>
+
+          {/* Avatar with first letter of username, capitalized */}
+          <Avatar>{authUser.charAt(0).toUpperCase()}</Avatar>
         </div>
-    );
+      )}
+
+      {/* ------------------ Content Section ------------------ */}
+      {/* 
+        <Outlet> is where child routes are rendered.
+        Example:
+          If the user clicks "Products", 
+          React Router will load the <Products /> page inside this Outlet.
+      */}
+      <Outlet />
+    </div>
+  );
 };
 
 export default Layout;
--- bakery/src/components/servicePages/Logout.tsx
+++ bakery/src/components/servicePages/Logout.tsx
@@ -2,15 +2,31 @@ import { useDispatch } from "react-redux";
 import { resetAuthUser } from "../../redux/slices/AuthSlice.ts";
 
 const Logout = () => {
+  /**
+   * useDispatch:
+   * - React-Redux hook that gives us access to the dispatch function.
+   * - We use dispatch to send actions to the Redux store.
+   * - Here we’ll use it to trigger "resetAuthUser" and clear the auth state.
+   */
   const dispatch = useDispatch();
 
+  /**
+   * handleLogout:
+   * - Event handler for the logout button.
+   * - Calls the resetAuthUser() action.
+   * - This sets authUser = "" in Redux → meaning no user is logged in.
+   */
   const handleLogout = () => {
-    // Clear global auth state
+    // Clear global auth state (Redux)
     dispatch(resetAuthUser());
+
+    // (Optional) You could also redirect the user back to login page here
+    // using React Router's useNavigate().
   };
 
   return (
     <div>
+      {/* Button that logs the user out */}
       <button onClick={handleLogout}>Logout</button>
     </div>
   );
