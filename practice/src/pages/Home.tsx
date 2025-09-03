8ccb06504a5820309ee2677f02fa9c9c0fe7c46e
Appile1 <ahmadsiddiquej3@gmail.com>
2025-09-03 21:17:00 +0500

    another project for pratice

---
+++ practice/src/pages/Home.tsx
@@ -0,0 +1,67 @@
+import * as React from "react";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import CssBaseline from "@mui/material/CssBaseline";
+import Divider from "@mui/material/Divider";
+import FormLabel from "@mui/material/FormLabel";
+import FormControl from "@mui/material/FormControl";
+import Link from "@mui/material/Link";
+import TextField from "@mui/material/TextField";
+import Typography from "@mui/material/Typography";
+import Stack from "@mui/material/Stack";
+import MuiCard from "@mui/material/Card";
+import { styled } from "@mui/material/styles";
+
+const Card = styled(MuiCard)(({ theme }) => ({
+  display: "flex",
+  flexDirection: "column",
+  alignSelf: "center",
+  width: "100%",
+  padding: theme.spacing(4),
+  gap: theme.spacing(2),
+  margin: "auto",
+  [theme.breakpoints.up("sm")]: {
+    maxWidth: "450px",
+  },
+  boxShadow:
+    "hsla(220, 30%, 5%, 0.05) 0px 5px 15px 0px, hsla(220, 25%, 10%, 0.05) 0px 15px 35px -5px",
+  ...theme.applyStyles("dark", {
+    boxShadow:
+      "hsla(220, 30%, 5%, 0.5) 0px 5px 15px 0px, hsla(220, 25%, 10%, 0.08) 0px 15px 35px -5px",
+  }),
+}));
+
+const SignInContainer = styled(Stack)(({ theme }) => ({
+  height: "calc((1 - var(--template-frame-height, 0)) * 100dvh)",
+  minHeight: "100%",
+  padding: theme.spacing(2),
+  [theme.breakpoints.up("sm")]: {
+    padding: theme.spacing(4),
+  },
+}));
+
+const Home = () => {
+  return (
+    <SignInContainer>
+      <CssBaseline />
+      <Card>
+        <Typography variant="h5">Welcome to the Home Page</Typography>
+        <Divider />
+        <FormControl>
+          <FormLabel>Email</FormLabel>
+          <TextField variant="outlined" fullWidth />
+        </FormControl>
+        <FormControl>
+          <FormLabel>Password</FormLabel>
+          <TextField variant="outlined" type="password" fullWidth />
+        </FormControl>
+        <Button variant="contained" color="primary">
+          Sign In
+        </Button>
+        <Link href="#">Forgot Password?</Link>
+      </Card>
+    </SignInContainer>
+  );
+};
+
+export default Home;
