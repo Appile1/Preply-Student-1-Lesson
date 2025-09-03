7dda160de4b750ff9014965b266253307949fae5
Appile1 <ahmadsiddiquej3@gmail.com>
2025-09-03 21:56:55 +0500

    back to home page

--- bakery/src/redux/slices/AuthSlice.ts
+++ bakery/src/redux/slices/AuthSlice.ts
@@ -1,19 +1,19 @@
-import {createSlice} from "@reduxjs/toolkit";
+import { createSlice } from "@reduxjs/toolkit";
 
 const authSlice = createSlice({
-    name: 'auth',
-    initialState: {
-        authUser: ""
+  name: "auth",
+  initialState: {
+    authUser: "",
+  },
+  reducers: {
+    setAuthUser: (state, action) => {
+      state.authUser = action.payload;
     },
-    reducers: {
-        setAuthUser: (state, action) => {
-            state.authUser = action.payload
-        },
-        resetAuthUser: state => {
-            state.authUser = ""
-        }
-    }
+    resetAuthUser: (state) => {
+      state.authUser = "";
+    },
+  },
 });
 
-export const {setAuthUser, resetAuthUser} = authSlice.actions;
+export const { setAuthUser, resetAuthUser } = authSlice.actions;
 export const authReducer = authSlice.reducer;
--- bakery/src/utils/constants.ts
+++ bakery/src/utils/constants.ts
@@ -1,17 +1,18 @@
-import type {NavItemType} from "./app-types.ts";
-import {Paths} from "./paths.ts";
+import type { NavItemType } from "./app-types.ts";
+import { Paths } from "./paths.ts";
 
 export const navItems: NavItemType[] = [
-    {route: Paths.HOME, itemName: "Home"},
-    {route: Paths.PRODUCTS, itemName: "Products"},
-    {route: Paths.CUSTOMERS, itemName: "Customers"},
-    {route: Paths.ORDERS, itemName: "Orders"},
-    {route: Paths.CART, itemName: "Shopping Cart"},
-    {route: Paths.LOGIN, itemName: "Login"},
-    {route: Paths.LOGOUT, itemName: "Logout"}
-]
+  { route: Paths.HOME, itemName: "Home" },
+  { route: Paths.PRODUCTS, itemName: "Products" },
+  { route: Paths.CUSTOMERS, itemName: "Customers" },
+  { route: Paths.ORDERS, itemName: "Orders" },
+  { route: Paths.CART, itemName: "Shopping Cart" },
+  { route: Paths.LOGIN, itemName: "Login" },
+  { route: Paths.LOGOUT, itemName: "Logout" },
+];
 
 export const productItems: NavItemType[] = [
-    {route: Paths.DAIRY, itemName: "Dairy"},
-    {route: Paths.BREAD, itemName: "Bread"},
-]
+  { route: Paths.DAIRY, itemName: "Dairy" },
+  { route: Paths.BREAD, itemName: "Bread" },
+  { route: Paths.HOME, itemName: "Back to Home" },
+];
