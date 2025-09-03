5d9359f56cdd2a55896d3d4d488ad16019974e85
Appile1 <ahmadsiddiquej3@gmail.com>
2025-09-03 21:44:29 +0500

    new projkect

---
+++ bakery/src/utils/paths.ts
@@ -0,0 +1,12 @@
+export enum Paths {
+    HOME = '/',
+    ORDERS = 'orders',
+    PRODUCTS = 'products',
+    CUSTOMERS = 'customers',
+    CART = 'cart',
+    BREAD = 'bread',
+    DAIRY = 'dairy',
+    ERROR = 'error',
+    LOGIN = 'login',
+    LOGOUT = 'logout'
+}
